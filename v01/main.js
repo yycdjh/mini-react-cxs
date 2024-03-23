b63e3f8997d67f838f96d14c0e3c8eb06caa376e
JD <591883039@qq.com>
2024-03-23 16:28:18 +0800

    fix: 提取为App引入

--- v01/main.js
+++ v01/main.js
@@ -1,5 +1,4 @@
 import ReactDom from "./core/ReactDom.js";
-import React from "./core/React.js";
+import App from "./App.js";
 
-const App = React.createElement("div", { id: "app" }, "hi-mini-react1");
 ReactDom.createRoot(document.querySelector("#root")).render(App);
