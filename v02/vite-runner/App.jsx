cec1aa86aa22f3fd6873971ee462148cdf4c7b4b
JD <jdwen@cfss.net.cn>
2024-03-23 20:44:00 +0800

    feat: 测试自定义react名字

--- v02/vite-runner/App.jsx
+++ v02/vite-runner/App.jsx
@@ -1,6 +1,11 @@
-import React from "./core/React.js";
+ //**@jsx JDReact.createElement */
+import JDReact   from "./core/React.js";
 
 // const App = React.createElement("div", { id: "app" }, "hi-mini-react");
 const App = <div>hi-mini-react12</div>
+function AppOne() {
+  return <div>hi-mini-react</div>;
+}
 
+console.log(AppOne)
 export default App;
