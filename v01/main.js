3de7f57e560c7119bc09e97f4c6bb3a407257704
JD <591883039@qq.com>
2024-03-23 15:24:43 +0800

    feat: 通过js object实现

--- v01/main.js
+++ v01/main.js
@@ -1,8 +1,32 @@
 // v1
-const dom = document.createElement("div");
-dom.id = "app";
+// const dom = document.createElement("div");
+// dom.id = "app";
+// document.querySelector("#root").append(dom);
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = "app";
+// dom.append(textNode);
+
+// v2 js object
+const textEl = {
+  type: "TEXT_ELEMENT",
+  props: {
+    nodeValue: "app",
+    children: [],
+  },
+};
+
+const el = {
+  type: "div",
+  props: {
+    id: "app",
+    children: [textEl],
+  },
+};
+const dom = document.createElement(el.type);
+dom.id = el.props.id;
 document.querySelector("#root").append(dom);
 
 const textNode = document.createTextNode("");
-textNode.nodeValue = "app";
+textNode.nodeValue = textEl.props.nodeValue;
 dom.append(textNode);
