511004ae3cf0a539684a6bce0d25263e1544453c
JD <jdwen@cfss.net.cn>
2024-03-25 20:58:42 +0800

    feat: init v06

---
+++ v06/vite-runner/App.jsx
@@ -0,0 +1,14 @@
+import React   from "./core/React.js";
+
+// const App = React.createElement("div", { id: "app" }, "hi-mini-react");
+const App = <div>
+  <div>111
+    <div>hi-mini-react12</div>
+  </div>
+</div>
+function AppOne() {
+  return <div>hi-mini-react</div>;
+}
+
+console.log(AppOne)
+export default App;
