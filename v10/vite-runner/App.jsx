7b5343dc49a3cddc4b3cf56f42728b8aef5c1775
JD <jdwen@cfss.net.cn>
2024-03-28 21:34:04 +0800

    feat: diff-删除多余的老节点

---
+++ v10/vite-runner/App.jsx
@@ -0,0 +1,35 @@
+import React from "./core/React.js";
+
+let showBar = false
+function Counter() {
+  // const foo = <div>foo</div>
+  const foo = <div>foo
+    <div>child</div>
+    <div>child2</div>
+
+  </div>
+  const bar = <div>bar</div>
+
+  function handleShowBar() {
+    showBar = !showBar
+    React.update()
+  }
+  return <div>
+    Counter
+    <button onClick={handleShowBar}>showBar</button>
+    <div>{showBar ? bar : foo}</div>
+
+  </div>
+}
+
+function App() {
+  return (
+    <div>
+      hi-mini-react
+      <Counter ></Counter>
+
+    </div>
+  )
+}
+
+export default App;
