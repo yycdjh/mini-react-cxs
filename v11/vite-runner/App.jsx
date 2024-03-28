5e9d4da64a10da1407d018509b257e4801da5b43
JD <jdwen@cfss.net.cn>
2024-03-28 22:22:55 +0800

    feat: 解决edge case

---
+++ v11/vite-runner/App.jsx
@@ -0,0 +1,30 @@
+import React from "./core/React.js";
+
+let showBar = false
+function Counter() {
+
+  const bar = <div>bar</div>;
+
+  function handleShowBar() {
+    showBar = !showBar;
+    React.update()
+  }
+  return (
+    <div>
+      counter
+      {showBar && bar}
+      <button onClick={handleShowBar}>showBar</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      hi-mini-react
+      <Counter></Counter>
+    </div>
+  )
+}
+
+export default App;
