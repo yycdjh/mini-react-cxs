58e153e7ef76f199de93fe0c038c99b9902c7824
JD <jdwen@cfss.net.cn>
2024-03-28 21:19:02 +0800

    feat：diif-实现创建新的、删除旧的

---
+++ v09/vite-runner/App.jsx
@@ -0,0 +1,32 @@
+import React from "./core/React.js";
+
+let showBar = false
+function Counter() {
+  // const foo = <div>foo</div>
+  function Foo() {
+    return <div>foo</div>
+  }
+  const bar = <p>bar</p>
+
+  function handleShowBar() {
+    showBar = !showBar
+    React.update()
+  }
+  return <div>
+    Counter
+    <div>{showBar ? bar : <Foo></Foo>}</div>
+    <button onClick={handleShowBar}>showBar</button>
+  </div>
+}
+
+function App() {
+  return (
+    <div>
+      hi-mini-react
+      <Counter ></Counter>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ v09/vite-runner/core/React.js
@@ -0,0 +1,250 @@
+// v3 通过动态创建el实现
+// 动态创建TextElement
+function createTextNode(value) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: value,
+      children: [],
+    },
+  };
+}
+
+// 动态创建Element
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        const isTextNode =
+          typeof child === "string" || typeof child === "number";
+        return isTextNode ? createTextNode(child) : child;
+      }),
+    },
+  };
+}
+
+function render(el, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+
+  nextWorkOfUnit = wipRoot;
+}
+
+// work in progress
+let wipRoot = null;
+let currentRoot = null;
+let nextWorkOfUnit = null;
+let deletions = [];
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (!shouldYield && nextWorkOfUnit) {
+    // run task
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  if (!nextWorkOfUnit && wipRoot) {
+    commitRoot();
+  }
+
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function commitRoot() {
+  deletions.forEach(commitDeletion);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+  deletions = [];
+}
+
+function commitDeletion(fiber) {
+  if (fiber.dom) {
+    let fiberParent = fiber.parent;
+    while (!fiberParent.dom) {
+      fiberParent = fiberParent.parent;
+    }
+    fiberParent.dom.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child);
+  }
+}
+
+function commitWork(fiber) {
+  if (!fiber) return;
+
+  let fiberParent = fiber.parent;
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent;
+  }
+
+  if (fiber.effectTag === "update") {
+    updateProps(fiber.dom, fiber.props, fiber.alternate?.props);
+  } else if (fiber.effectTag === "placement") {
+    if (fiber.dom) {
+      fiberParent.dom.append(fiber.dom);
+    }
+  }
+
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, nextProps, prevProps) {
+  // Object.keys(props).forEach((key) => {
+  //   if (key !== "children") {
+  //     if (key.startsWith("on")) {
+  //       const eventType = key.slice(2).toLocaleLowerCase();
+  //       dom.addEventListener(eventType, props[key]);
+  //     } else {
+  //       dom[key] = props[key];
+  //     }
+  //   }
+  // });
+  //   - 3、新老节点对比
+  // - - 1、重构 updateProps
+  // - - - 1、old 有 new 没有 删除
+  Object.keys(prevProps).forEach((key) => {
+    if (key !== "children") {
+      if (!(key in nextProps)) {
+        dom.removeAttribute(key);
+      }
+    }
+  });
+  // - - - 2、new 有 old 没有 添加
+  // - - - 3、new 有 old 有 修改
+  Object.keys(nextProps).forEach((key) => {
+    if (key !== "children") {
+      if (nextProps[key] !== prevProps[key]) {
+        if (key.startsWith("on")) {
+          const eventType = key.slice(2).toLocaleLowerCase();
+          dom.removeEventListener(eventType, prevProps[key]);
+          dom.addEventListener(eventType, nextProps[key]);
+        } else {
+          dom[key] = nextProps[key];
+        }
+      }
+    }
+  });
+}
+
+let newFiber;
+function reconcileChildren(fiber, children) {
+  let oldFiber = fiber.alternate?.child;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const isSameType = oldFiber && oldFiber.type === child.type;
+
+    if (isSameType) {
+      // 更新
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: oldFiber.dom,
+        effectTag: "update",
+        alternate: oldFiber,
+      };
+    } else {
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: null,
+        effectTag: "placement",
+      };
+
+      if (oldFiber) {
+        console.log("should delete", oldFiber);
+        deletions.push(oldFiber);
+      }
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber.type);
+  }
+  updateProps(fiber.dom, fiber.props, {});
+
+  const children = fiber.props.children;
+  // 转换链表 设置指针
+  reconcileChildren(fiber, children);
+}
+
+function performWorkOfUnit(fiber) {
+  const isFunctionComponent = typeof fiber.type === "function";
+
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
+  }
+
+  // 返回下一个任务
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+  // return fiber.parent?.sibling;
+}
+
+function update() {
+  wipRoot = {
+    dom: currentRoot.dom,
+    props: currentRoot.props,
+    alternate: currentRoot,
+  };
+
+  nextWorkOfUnit = wipRoot;
+}
+
+const React = {
+  update,
+  render,
+  createElement,
+};
+
+export default React;
