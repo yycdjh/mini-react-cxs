167102c339aefbe73849741f9de2b53d4c95efc7
JD <jdwen@cfss.net.cn>
2024-03-29 21:07:37 +0800

    feat: 批量执行 action

---
+++ v14/vite-runner/App.jsx
@@ -0,0 +1,40 @@
+import React from "./core/React.js";
+
+function Foo() {
+  const [count, setCount] = React.useState(10)
+  const [bar, setBar] = React.useState('bar')
+
+  function handleClick() {
+    setCount((c) => c + 1)
+    // setBar((b) => b + 'bar')
+    setBar('barbar')
+
+  }
+  return (
+    <div>
+      <h1>foo</h1>
+      {count}
+      <div></div>
+      {bar}
+      <div></div>
+
+      <button onClick={handleClick}>click</button>
+    </div>
+  )
+}
+
+
+function App() {
+  function handleClick() {
+  }
+  return (
+    <div>
+      hi-mini-react
+      <button onClick={handleClick}>click</button>
+
+      <Foo></Foo>
+    </div>
+  )
+}
+
+export default App;
