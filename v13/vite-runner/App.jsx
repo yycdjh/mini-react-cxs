6b03ef8e02353ef9cca8849112f4ddf3a6f1230d
JD <jdwen@cfss.net.cn>
2024-03-29 20:57:20 +0800

    feat: 实现useState

---
+++ v13/vite-runner/App.jsx
@@ -0,0 +1,38 @@
+import React from "./core/React.js";
+
+function Foo() {
+  const [count, setCount] = React.useState(10)
+  const [bar, setBar] = React.useState('bar')
+
+  function handleClick() {
+    setCount((c) => c + 1)
+    setBar((b) => b + 'bar')
+  }
+  return (
+    <div>
+      <h1>foo</h1>
+      {count}
+      <div></div>
+      {bar}
+      <div></div>
+
+      <button onClick={handleClick}>click</button>
+    </div>
+  )
+}
+
+
+function App() {
+  function handleClick() {
+  }
+  return (
+    <div>
+      hi-mini-react
+      <button onClick={handleClick}>click</button>
+
+      <Foo></Foo>
+    </div>
+  )
+}
+
+export default App;
---
+++ v13/vite-runner/main.jsx
@@ -0,0 +1,5 @@
+import ReactDom from "./core/ReactDom.js";
+import React from "./core/React.js";
+import App from "./App.jsx";
+
+ReactDom.createRoot(document.querySelector("#root")).render(<App />);
